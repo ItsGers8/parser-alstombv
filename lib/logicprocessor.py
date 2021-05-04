1a7d6ed82df4b6abee36cab4a1f50b6449b6bcf6
tygos <tygo.steenbergen@student.hu.nl>
2021-05-04 12:39:08 +0200

    sorting function added

--- lib/logicprocessor.py
+++ lib/logicprocessor.py
@@ -10,9 +10,19 @@ def get_dictionary(file):
 
 # Function that processes the equations by checking what size they are and calling the according function
 def process(dictionary):
-    return dictionary
+    solvedEquationDict = dict()
+    for key in dictionary:
+        if len(dictionary[key]) > 2:
+            solvedEquationDict[key] = large_equation_solver(dictionary[key])
+        else:
+            solvedEquationDict[key] = one_line_solver(dictionary[key])
+    return solvedEquationDict
 
 
 # Function that generates the correct string based on the syntax of the output file
 def one_line_solver(equation):
+    return equation
+
+
+def large_equation_solver(equation):
     return equation
