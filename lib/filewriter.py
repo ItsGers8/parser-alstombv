3c61ff434c91c20f54bb5b85a07080288157cf96
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-05-18 12:25:47 +0200

    Updated filewriter.py to be better formatted and work a bit nicer

--- lib/filewriter.py
+++ lib/filewriter.py
@@ -17,13 +17,12 @@ def write(dictionary, file):
     list_with_values = get_values_list(dictionary)
 
     open(file, 'w')
-    while index <= len(list_with_keys)-1:
-        write_file = open(file, "a")
-
-        res = '\n* {0} \n{1}    {2} = \n                        ({3})'.format(num, 'BOOL', str(list_with_keys[index]),
-                                                                              str(list_with_values[index]))
+    write_file = open(file, 'a')
+    while index <= len(list_with_keys) - 1:
+        res = '\n* {0} \n{1}\t{2} = \n\t\t\t\t\t\t({3})'\
+            .format(num, 'BOOL', str(list_with_keys[index]), str(list_with_values[index]))
         write_file.write(res)
         num = num + 1
         index = index + 1
 
-        write_file.close()
+    write_file.close()
